c03d24709b67fcf24aeaa5257023a0c12ba84416
daeunkwak <misodaeun3@naver.com>
2023-07-11 20:57:13 +0900

    feat: 애플로그인 의존성 추가

--- cider-api/src/main/java/com/cmc/domains/member/controller/MemberController.java
+++ cider-api/src/main/java/com/cmc/domains/member/controller/MemberController.java
@@ -5,17 +5,19 @@ import com.cmc.domains.member.dto.response.MemberUpdateResDto;
 import com.cmc.domains.member.service.MemberService;
 import com.cmc.global.resolver.RequestMemberId;
 import com.cmc.member.Member;
+import com.cmc.oauth.dto.request.OauthReqDto;
+import com.cmc.oauth.dto.response.ResponseJwtTokenDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @Slf4j
@@ -35,4 +37,14 @@ public class MemberController {
         Member member = memberService.updateMember(memberId, request.getMemberGender(), request.getMemberBirth(), request.getInterestChallenge());
         return ResponseEntity.ok(MemberUpdateResDto.from(member));
     }
+
+//    @Tag(name = "members")
+//    @GetMapping(value = "/nickname")
+//    @Operation(summary = "닉네임 랜덤 생성 api")
+//    public ResponseEntity<ResponseJwtTokenDto> createName(@Valid @RequestBody OauthReqDto oauthReqDto, HttpServletRequest httpServletRequest) throws JsonProcessingException {
+//
+//        return ResponseEntity.ok(jwtTokenDto);
+//    }
+
+
 }
